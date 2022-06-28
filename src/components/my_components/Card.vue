1348427521b7c2f7b423caffdc8712b7b8559e31
Laura <lauragarciacastelao@hotmail.com>
2022-06-28 15:42:51 +0200

    Add properties of superheroes

--- src/components/my_components/Card.vue
+++ src/components/my_components/Card.vue
@@ -1,47 +1,50 @@
 <script>
-export default{
+export default {
   props: {
-    heroe: Object
-  }
+    heroe: Object,
+  },
 }
 </script>
 
-
 <template>
-  <div id="bomb" class="card mb-5 mt-5" style="width: 18rem">
-    <div class="d-flex justify-content-between">
-      <h5 class="card-title"> {{heroe.name}}</h5>
-      <h6>Power: 100</h6>
-    </div>
-    <img
-      src="https://vignette.wikia.nocookie.net/marvel/images/a/ab/A-Bomb_(Rick_Jones).jpg/revision/latest?cb=20110404170428&path-prefix=es"
-      class="card-img-top"
-      alt="..."
-    />
-    <ul class="list-group list-group-flush">
-      <li class="list-group-item">
-        <i class="fa-solid fa-brain" style="color: purple"></i> Intelligence {{heroe.intelligence}}
-      </li>
-      <li class="list-group-item">
-        <i class="fa-solid fa-hand-back-fist" style="color: chocolate"></i> {{heroe.strength}}
-        Strength
-      </li>
-      <li class="list-group-item">
-        <i class="fa-solid fa-person-running" style="color: green"></i> Speed: {{heroe.speed}}
-      </li>
-      <li class="list-group-item">
-        <i class="fa-solid fa-battery-quarter" style="color: blue"></i>
-        Durability {{heroe.durability}}
-      </li>
-      <li class="list-group-item">
-        <i class="fa-solid fa-bomb" style="color: red"></i>Combat {{heroe.combat}}
-      </li>
-    </ul>
+  <div class="d-flex justify-content-center">
+      <div id="bomb" class="card mb-5 mt-5" style="width: 18rem">
+        <div class="d-flex justify-content-between">
+          <h5 class="card-title">{{ heroe.name }}</h5>
+          <h6>Power: 100</h6>
+      </div>
+      <img
+        src="https://vignette.wikia.nocookie.net/marvel/images/a/ab/A-Bomb_(Rick_Jones).jpg/revision/latest?cb=20110404170428&path-prefix=es"
+        class="card-img-top"
+        alt="..."/>
 
-    <div class="card-body">
-      <a href="#" class="card-link">
-        <i class="fa-solid fa-heart" style="color: crimson"></i
-      ></a>
+      <ul class="list-group list-group-flush">
+           <li class="list-group-item">
+               <i class="fa-solid fa-brain" style="color: purple"></i> Intelligence:
+          {{ heroe.powerstats.intelligence }}
+           </li>
+           <li class="list-group-item">
+              <i class="fa-solid fa-hand-back-fist" style="color: chocolate"></i>
+          Strength: {{ heroe.powerstats.strength }}
+           </li>
+           <li class="list-group-item">
+              <i class="fa-solid fa-person-running" style="color: green"></i> Speed:
+          {{ heroe.powerstats.speed }}
+            </li>
+          <li class="list-group-item">
+              <i class="fa-solid fa-battery-quarter" style="color: blue"></i>
+          Durability: {{ heroe.powerstats.durability }}
+          </li>
+          <li class="list-group-item">
+              <i class="fa-solid fa-bomb" style="color: red"></i>Combat:
+          {{ heroe.powerstats.combat }}
+          </li>
+      </ul>
+
+       <div class="card-body">
+               <a href="#" class="card-link">
+              <i class="fa-solid fa-heart" style="color: crimson"></i> </a>
+           </div>
     </div>
   </div>
 </template>
